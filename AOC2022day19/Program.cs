859c78b8406a25b381c82a8cd95ddd2d7ecc69ad
HIR021419 <yann.lemetayer@epita.fr>
2025-06-10 17:16:56 +0200

    feat: Main

--- AOC2022day19/Program.cs
+++ AOC2022day19/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        (int, int) max = (-1, -1);
+
+        string[] input = Utils.OpenFile(args[1]);
+        Blueprint[] blueprints = Utils.Parse(input);
+        foreach (Blueprint blueprint in blueprints)
+        {
+            int res = TestBlueprint(blueprint);
+            if (res > max.Item2) max = (blueprint.Index, res);
+        }
+        
+        Console.WriteLine(max.Item1);
+    }
+
+    static int TestBlueprint(Blueprint blueprint)
+    {
+        int max = -1;
+        
+        List<State> previousStates = [];
+        List<State> currentStates = [new()];
+        for (int i = 0; i < 24; i++)
+        {
+            previousStates = currentStates.GetRange(0, currentStates.Count);
+            foreach (State state in previousStates)
+            {
+                foreach (Robot robot in blueprint.Robots)
+                {
+                    State? newState = robot.AddRobot(state);
+                    if (newState != null) currentStates.Add(newState);
+                }
+
+                currentStates.Add(state);
+            }
+            foreach (State state in currentStates) state.MineResources();
+
+            currentStates.Sort();
+        }
+        
+        return blueprint.Index * GetMax(currentStates.ToArray());
+    }
+
+    private static int GetMax(State[] states)
+    {
+        if (states.Length == 0) return 0;
+        int max = states[0].resources[4];
+        for (int i = 1; i < states.Length; i++)
+        {
+            if (states[i].resources[4] > max) max = states[i].resources[4];
+        }
+        return max;
     }
 }
--- AOC2022day19/State.cs
+++ AOC2022day19/State.cs
@@ -3,7 +3,7 @@
 public class State
 {
     public int[] resources { get; } = new int[5];
-    public int[] robots { get; } = new int[5];
+    public int[] robots { get; } = [1,0,0,0,0];
 
 
     public State Clone()
@@ -15,5 +15,13 @@ public class State
             newState.robots[i] = robots[i];
         }
         return newState;
+    }
+
+    public void MineResources()
+    {
+        for (int i = 0; i < resources.Length; i++)
+        {
+            resources[i] += robots[i];
+        }
     }
 }
